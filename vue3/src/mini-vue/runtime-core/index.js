4a20810ecabacf1aabd1891cd5bb36e99fcff139
lingfengjgf <lingfengjgf@163.com>
2023-05-13 14:41:06 +0800

    在mini-vue中应用reactive

--- vue3/src/mini-vue/runtime-core/index.js
+++ vue3/src/mini-vue/runtime-core/index.js
@@ -1,12 +1,22 @@
+import { reactive } from "../reactivity";
+
 export function createRenderer(options) {
   // render负责渲染组件内容
   const render = (rootComponent, selector) => {
     // 获取宿主
     const container = options.querySelector(selector);
-    // 渲染视图
-    const el = rootComponent.render.call(rootComponent.data());
-    // 追加到宿主
-    options.insert(el, container);
+
+    // 响应式处理
+    let observed = reactive(rootComponent.data());
+
+    // 定义更新函数
+    const componentUpdateFn = () => {
+      // 渲染视图
+      const el = rootComponent.render.call(observed);
+      // 追加到宿主
+      options.insert(el, container);
+    };
+    componentUpdateFn();
   };
 
   // 返回一个渲染器实例
